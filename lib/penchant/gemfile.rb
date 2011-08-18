8fac9351ced7d38d11c40cda00fdae238bb76196
John Bintz <john@coswellproductions.com>
2011-08-18 12:50:14 -0400

    start of spec work

--- lib/penchant/gemfile.rb
+++ lib/penchant/gemfile.rb
@@ -6,17 +6,29 @@ module Penchant
       @path = path
     end
 
+    def gemfile_path
+      file_in_path('Gemfile')
+    end
+
     def has_gemfile?
-      has_file_in_path?('Gemfile')
+      File.file?('Gemfile')
+    end
+
+    def gemfile_erb_path
+      file_in_path('Gemfile.erb')
     end
 
     def has_gemfile_erb?
-      has_file_in_path?('Gemfile.erb')
+      File.file?(gemfile_erb_path)
+    end
+
+    def environment
+      File.readlines(gemfile_path).first.strip[%r{environment: (.*)}, 1]
     end
 
     private
-    def has_file_in_path?(file)
-      File.file?(File.join(@path, file))
+    def file_in_path(file)
+      File.join(@path, file)
     end
   end
 end
